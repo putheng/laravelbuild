ed1acd965f30be2c948f686153cfe07251ae32a6
Putheng <puthengemail@gmail.com>
2018-09-28 14:19:01 +0700

    kk

--- app/exec/user.py
+++ app/exec/user.py
@@ -3,8 +3,9 @@ import os
 import sys
 #sys.argv[1]
 
-os.system('useradd -m -d /home/'+ sys.argv[1] +' '+ sys.argv[1] +'');
-os.system("usermod -a -G git "+ sys.argv[1])
+os.system("mkdir "+ sys.argv[1])
+#os.system('useradd -m -d /home/'+ sys.argv[1] +' '+ sys.argv[1] +'');
+#os.system("usermod -a -G git "+ sys.argv[1])
 
-os.system("sudo mkdir -p /homes/"+ sys.argv[1])
-os.system("sudo chown -R "+ sys.argv[1] +":git /homes/"+ sys.argv[1])
+#os.system("sudo mkdir -p /homes/"+ sys.argv[1])
+#os.system("sudo chown -R "+ sys.argv[1] +":git /homes/"+ sys.argv[1])
